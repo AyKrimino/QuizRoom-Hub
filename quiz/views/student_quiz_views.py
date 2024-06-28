fbb88f6e570cd82a6106cb24dfc7d1d6ce738437
AyKrimino <ayoub.krimi14@gmail.com>
2024-06-28 19:20:26 +0100

    quiz/view/student_quiz_views for quiz

---
+++ quiz/views/student_quiz_views.py
@@ -0,0 +1,49 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
+from rest_framework.views import APIView
+
+from classroom.models import Classroom
+from classroom.permissions import IsClassroomMember, IsStudent
+from quiz.models import StudentQuiz
+from quiz.serializers import StudentAnswerSerializer, StudentQuizSerializer
+
+
+class StudentAnswerCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated, IsClassroomMember, IsStudent]
+
+    def post(self, request, *args, **kwargs):
+        self.check_permissions(request)
+
+        serializer = StudentAnswerSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class StudentQuizCreateAPIView(CreateAPIView):
+    queryset = StudentQuiz.objects.all()
+    serializer_class = StudentQuizSerializer
+    permission_classes = [IsAuthenticated, IsClassroomMember, IsStudent]
+
+
+class StudentQuizListAPIView(ListAPIView):
+    serializer_class = StudentQuizSerializer
+    permission_classes = [IsAuthenticated, IsClassroomMember]
+
+    def get_queryset(self):
+        user = self.request.user
+        classroom_id = self.request.GET.get("classroom_id")
+
+        if hasattr(user, "student_profile"):
+            return StudentQuiz.objects.filter(student=user.student_profile)
+        else:  # user is a teacher
+            try:
+                classroom = Classroom.objects.get(id=classroom_id)
+            except Classroom.DoesNotExist:
+                raise ValidationError(_("Classroom does not exist."))
+
+            return StudentQuiz.objects.filter(quiz__classroom=classroom)
