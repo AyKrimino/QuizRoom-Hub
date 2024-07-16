ae7490ee59dc839e131777e606e37b9d22239116
AyKrimino <ayoub.krimi14@gmail.com>
2024-07-16 21:21:56 +0100

    permissions for post/

--- post/permissions.py
+++ post/permissions.py
@@ -3,8 +3,36 @@ from classroom.permissions import IsClassroomOwner
 
 
 class IsCommentAuthor(BasePermission):
+    """
+    Custom permission to only allow authors of a comment to edit it.
+
+    Methods: - has_object_permission(request, view, obj): Returns `True` if the user making the request is the author
+    of the comment, `False` otherwise.
+
+    Usage:
+    Apply this permission to views where you want to restrict access to actions (such as update or delete) to the author of the comment.
+    """
+
     def has_object_permission(self, request, view, obj):
-        pass
+        """
+        Check if the user making the request is the author of the comment.
+
+        Args:
+        - request: The HTTP request object.
+        - view: The view that is being accessed.
+        - obj: The object being accessed (in this case, a Comment instance).
+
+        Returns:
+        - bool: `True` if the request.user is the author of the comment, `False` otherwise.
+        """
+        return request.user == obj.user
 
 
+# Composite permission combining IsClassroomOwner and IsCommentAuthor permissions
 IsClassroomOwnerOrCommentAuthor = IsClassroomOwner | IsCommentAuthor
+"""
+Composite permission to allow access if the user is either the owner of the classroom or the author of the comment.
+
+Usage:
+Apply this permission to views where you want to restrict access to actions (such as delete) to either the owner of the classroom or the author of the comment.
+"""
